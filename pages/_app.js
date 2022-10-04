309668d1d3546c878cb437f1eacdaece4c636f88
ErikMurmi <erikmurmi@gmail.com>
2022-10-04 17:14:19 -0500

    First commit and  database connection working

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <>
+    <heaader>
+    <title>UsCrud </title>
+        <meta name="description" content="Developed by Erik Murminacho" />
+        <link rel="icon" href="/favicon.ico" />
+    </heaader>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
--- pages/api/users.js
+++ pages/api/users.js
@@ -1,5 +1,9 @@
+import {dbConnect} from '../../utils/mongoose'
+
+dbConnect()
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req,res){
-  res.send('Res test text')
-}
+    res.send('Res test text')
+  }
